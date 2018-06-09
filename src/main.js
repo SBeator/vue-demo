60f30a90fb19099f165e1b8168aea3134488a680
Xingxin Zeng <star_yes@qq.com>
2018-06-09 16:17:55 +0800

    Know how to override theme

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import iView from 'iview'
-import 'iview/dist/styles/iview.css'
+import './theme/index.less'
 
 Vue.config.productionTip = false
 
