d617919a77d830770cb3c94fc8f73ff247f53f72
Xingxin Zeng <star_yes@qq.com>
2018-06-09 16:51:41 +0800

    Make menu works

--- src/components/PageLayout.vue
+++ src/components/PageLayout.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="container">
+
     <div class="layout">
       <Layout>
         <Header>
           <slot name="header"></slot>
         </Header>
-        <Layout>
+        <Layout :style="{minHeight: '100vh'}">
           <Sider hide-trigger>
             <slot name="navigate"></slot>
-
           </Sider>
           <Content>
             <slot></slot>
-
           </Content>
         </Layout>
       </Layout>
