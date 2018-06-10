10af6d25bb0caa9ec7d1c7a2e810ba2ede832967
Xingxin Zeng <star_yes@qq.com>
2018-06-10 10:43:04 +0800

    Override style for menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,21 @@
 <template>
   <header class="header">
     <h1>后台管理系统</h1>
+    <div class="profile">
+      <Avatar icon="person" />
+    </div>
   </header>
 </template>
 
 <style scoped lang="less">
-h1 {
+.header h1 {
   color: #ffffff;
   font-size: 14px;
   font-weight: normal;
+  float: left;
+}
+
+.profile {
+  float: right;
 }
 </style>
--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -14,7 +14,7 @@
         账号管理
       </template>
       <MenuItem name="/accounts/users">用户管理</MenuItem>
-      <MenuItem name="/accounts/texies">出租车管理</MenuItem>
+      <MenuItem name="/accounts/taxies">出租车管理</MenuItem>
       <MenuItem name="/accounts/companies">订车公司管理</MenuItem>
     </Submenu>
     <Submenu name="/accounts">
@@ -22,7 +22,7 @@
         账号管理
       </template>
       <MenuItem name="/accounts/users">用户管理</MenuItem>
-      <MenuItem name="/accounts/texies">出租车管理</MenuItem>
+      <MenuItem name="/accounts/taxies">出租车管理</MenuItem>
       <MenuItem name="/accounts/companies">订车公司管理</MenuItem>
     </Submenu>
   </Menu>
@@ -41,8 +41,33 @@ export default {
   }
 }
 </script>
-<style lang="less" scoped>
+<style lang="less">
+@import '../theme/color.less';
+
 .menu {
   height: 100%;
+  font-size: 12x;
+}
+
+.ivu-menu-light.ivu-menu-vertical {
+  .ivu-menu-item,
+  .ivu-menu-submenu-title {
+    border-left: 0px solid @primary-color;
+    transition: all 0.2s linear;
+  }
+  .ivu-menu-item-active {
+    &.ivu-menu-item,
+    .ivu-menu-submenu-title {
+      border-right-color: rgba(0, 0, 0, 0);
+      border-left-width: 6px;
+      padding-left: 18px;
+      color: @primary-color;
+    }
+  }
+
+  .ivu-menu-submenu .ivu-menu-item-active.ivu-menu-item {
+    border-left-width: 0;
+    padding-left: 24px;
+  }
 }
 </style>
