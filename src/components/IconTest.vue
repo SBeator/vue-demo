9aa0e71eacd9b6b1f3343f5a186dde46f6318661
Xingxin Zeng <star_yes@qq.com>
2018-06-20 22:38:25 +0800

    Use more info in icon test

--- src/components/IconTest.vue
+++ src/components/IconTest.vue
@@ -4,6 +4,9 @@
     <Icon type="new-draw" :size="16" color="red"></Icon>
     <Icon type="new-draw" class="aaa"></Icon>
     <Icon type="new-draw"></Icon>
+    <Icon type="ionic" :size="20"></Icon>
+    <Icon type="ionic" :size="16" color="red"></Icon>
+    <Icon type="ionic" class="aaa"></Icon>
     <Icon type="ionic"></Icon>
     <Icon type="arrow-up-a"></Icon>
     <Icon type="arrow-right-a"></Icon>
