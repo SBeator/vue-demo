23a491a7b8f77e8af5158b63bfe7a738c0ecf435
Xingxin Zeng <star_yes@qq.com>
2018-07-14 21:29:16 +0800

    Add queries support

--- src/pages/PlaceholderPage.vue
+++ src/pages/PlaceholderPage.vue
@@ -1,11 +1,19 @@
 <template>
-  <h1>{{pageName}}</h1>
+  <div>
+    <h1>{{pageName}}</h1>
+    <p v-for="key in Object.keys(queries)" :key="key">
+      {{key}}: {{queries[key]}}
+    </p>
+  </div>
 </template>
 <script>
 export default {
   computed: {
     pageName() {
       return this.$route.name
+    },
+    queries() {
+      return this.$route.query
     }
   }
 }
